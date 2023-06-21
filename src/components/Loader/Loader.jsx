af5c73b28cbcda8e7f7ad609c87fb64effde1973
Roland Dąbrowski <roli.dabrowski@gmail.com>
2023-06-21 13:20:13 +0200

    added custom loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,19 +1,13 @@
-import React from 'react';
-import ContentLoader from 'react-content-loader';
+import { Dna } from 'react-loader-spinner';
 
 export const MyLoader = props => (
-  <ContentLoader
-    speed={2}
-    width={400}
-    height={460}
-    viewBox="0 0 400 460"
-    backgroundColor="#f3f3f3"
-    foregroundColor="#ecebeb"
+  <Dna
+    visible={true}
+    height="80"
+    width="80"
+    ariaLabel="dna-loading"
+    wrapperStyle={{}}
+    wrapperClass="dna-wrapper"
     {...props}
-  >
-    <circle cx="31" cy="31" r="15" />
-    <rect x="58" y="18" rx="2" ry="2" width="140" height="10" />
-    <rect x="58" y="34" rx="2" ry="2" width="140" height="10" />
-    <rect x="0" y="60" rx="2" ry="2" width="400" height="400" />
-  </ContentLoader>
+  ></Dna>
 );
